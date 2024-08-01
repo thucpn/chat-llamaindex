442464eb8e4f13cf476e7008dbc65ce152a18e0f
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-08-01 18:37:28 +0700

    move checkExists inside getIndex

--- app/api/chat/engine/index.ts
+++ app/api/chat/engine/index.ts
@@ -1,20 +1,20 @@
-import { SimpleDocumentStore, VectorStoreIndex } from "llamaindex";
-import { storageContextFromDefaults } from "llamaindex/storage/StorageContext";
-import { STORAGE_CACHE_DIR } from "@/cl/app/api/chat/engine/shared";
+import { VectorStoreIndex } from "llamaindex";
+import { MilvusVectorStore } from "llamaindex/storage/vectorStore/MilvusVectorStore";
+import {
+  checkRequiredEnvVars,
+  getMilvusClient,
+} from "@/cl/app/api/chat/engine/shared";
 
 export async function getDataSource(datasource: string) {
   console.log(`Using datasource: ${datasource}`);
-  const storageContext = await storageContextFromDefaults({
-    persistDir: `${STORAGE_CACHE_DIR}/${datasource}`,
+  checkRequiredEnvVars({ checkCollectionEnv: false }); // Do not check for collection env var
+  const milvusClient = getMilvusClient();
+  const isCollectionExist = await milvusClient.hasCollection({
+    collection_name: datasource,
   });
-
-  const numberOfDocs = Object.keys(
-    (storageContext.docStore as SimpleDocumentStore).toDict(),
-  ).length;
-  if (numberOfDocs === 0) {
+  if (!isCollectionExist.value) {
     return null;
   }
-  return await VectorStoreIndex.init({
-    storageContext,
-  });
+  const store = new MilvusVectorStore({ milvusClient, collection: datasource });
+  return await VectorStoreIndex.fromVectorStore(store);
 }
